7d94f7ef1ca3e336bacfb63599cadf0546b9e405
Abrar <abrar.haq535@gmail.com>
2020-05-05 01:29:49 +0600

    done commit

--- src/main/java/io/javabrains/springsecurityjwt/services/MyUserDetailsService.java
+++ src/main/java/io/javabrains/springsecurityjwt/services/MyUserDetailsService.java
@@ -14,5 +14,13 @@ public class MyUserDetailsService implements UserDetailsService {
         System.out.println("ami new "+userName);
         return new User(Integer.toString(1),"foo",new ArrayList<>());
     }
-    //hello 
+    //hello
+
+
+
+
+
+
+
+    //feature-d
 }
